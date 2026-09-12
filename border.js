cc74e97864bea82505910d6f9899b961073a7a09
Hailemariam tesfaye <hailedev5@gmail.com>
2026-09-12 16:35:10 +0300

    Update border.js

--- border.js
+++ border.js
@@ -1,1 +1,18 @@
-hi
+function getValue() {
+  var value = document.getElementById("color-js").value;
+  var alertMessage = "your selected color code is >" + value;
+
+  // Create a temporary hidden textarea
+  var tempInput = document.createElement("textarea");
+  tempInput.value = alertMessage;
+  document.body.appendChild(tempInput);
+
+  // Select and copy
+  tempInput.select();
+  document.execCommand("copy");
+
+  // Clean up
+  document.body.removeChild(tempInput);
+
+  alert("Copied: " + alertMessage);
+}
